0ce9d745e58b69a5b416fc5743e9775c6c8b4a28
5gsilver <naruoto42@gmail.com>
2024-10-04 15:21:00 +0900

    chore: remove unnecessary codes

---
+++ joongo-market/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "중고마켓",
+  description: "중고거래 플랫폼",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body
+        className={"antialiased"}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
